e1e0306827075279c8b8d1582b0a6a89e609a3c7
Marko Banusic <mbanusic@gmail.com>
2018-01-03 09:41:06 +0100

    Home fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,9 @@
                 @foreach($months as $month)
                     <th><a href="{{ url(sprintf('%02d%d', $month, 2017)) }}">{{ date('M', mktime(0, 0, 0, $month)) }}</a> / <a href="{{ url('cat/'.sprintf('%02d%d', $month, 2017)) }}">Cat</a></th>
                 @endforeach
+                @foreach($months as $month)
+                    <th><a href="{{ url(sprintf('%02d%d', $month, 2018)) }}">{{ date('M', mktime(0, 0, 0, $month)) }}</a> / <a href="{{ url('cat/'.sprintf('%02d%d', $month, 2018)) }}">Cat</a></th>
+                @endforeach
             </tr>
             </thead>
             <tbody>
@@ -24,13 +27,19 @@
                     <td>{{ $user->stats->sum('posts') }}</td>
 
                     @foreach($months as $month)
-                        <td>{{ $user->stats->where('month', $month)->sum('posts') }}</td>
+                        <td>{{ $user->stats->where('month', $month)->where('year', 2017)->sum('posts') }}</td>
+                    @endforeach
+                    @foreach($months as $month)
+                        <td>{{ $user->stats->where('month', $month)->where('year', 2018)->sum('posts') }}</td>
                     @endforeach
                 </tr>
                 <tr>
                     <td>{{ $user->stats->sum('chars') }}</td>
                     @foreach($months as $month)
-                        <td>{{ $user->stats->where('month', $month)->sum('chars') }}</td>
+                        <td>{{ $user->stats->where('month', $month)->where('year', 2017)->sum('chars') }}</td>
+                    @endforeach
+                    @foreach($months as $month)
+                        <td>{{ $user->stats->where('month', $month)->where('year', 2018)->sum('chars') }}</td>
                     @endforeach
                 </tr>
             @endforeach
